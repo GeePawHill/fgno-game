4c22fc8c9e6b09c8dc97602c46e63fb3bd0d61de
GeePaw Hill <geepawhill@geepawhill.org>
2022-07-30 12:14:14 -0400

    Makes raining sounds! Oh boy oh boy oh boy!

--- core/src/main/org/fgno/game/FgnoGame.kt
+++ core/src/main/org/fgno/game/FgnoGame.kt
@@ -15,28 +15,20 @@ class FgnoGame : ApplicationAdapter() {
     private lateinit var dropSound: Sound
     private lateinit var rainMusic: Music
 
-    var batch: SpriteBatch? = null
-    var img: Texture? = null
-
     override fun create() {
         dropImage = Texture(com.badlogic.gdx.Gdx.files.internal("droplet.png"))
         bucketImage  = Texture(Gdx.files.internal("bucket.png"))
         dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
         rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
 
-        batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+        rainMusic.isLooping=true
+        rainMusic.play()
     }
 
     override fun render() {
         ScreenUtils.clear(1f, 0f, 0f, 1f)
-        batch!!.begin()
-        batch!!.draw(img, 0f, 0f)
-        batch!!.end()
     }
 
     override fun dispose() {
-        batch!!.dispose()
-        img!!.dispose()
     }
 }
