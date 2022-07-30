cd7b15517d1104685ee03a8d4d83dc8bb72fd11c
GeePaw Hill <geepawhill@geepawhill.org>
2022-07-30 11:32:54 -0400

    Added test folder and finally got it to run tests.

---
+++ core/src/test/org/fgno/game/SomethingTest.kt
@@ -0,0 +1,11 @@
+package org.fgno.game
+
+import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
+
+class SomethingTest {
+    @Test
+    fun testSomething() {
+        assertThat(1).isEqualTo(2)
+    }
+}
