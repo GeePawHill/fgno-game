b7e3b58a660bdcab07b385e94f0f2f20435307b7
GeePaw Hill <geepawhill@geepawhill.org>
2022-07-29 22:04:27 -0400

    Basic libgdx project added.

---
+++ desktop/src/org/fgno/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package org.fgno.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import org.fgno.game.FgnoGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("fgno-game");
+		new Lwjgl3Application(new FgnoGame(), config);
+	}
+}
