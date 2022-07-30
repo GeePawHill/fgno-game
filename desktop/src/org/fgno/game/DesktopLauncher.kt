184edf0932c8d3a8685b12b3c1c7304d6b17290f
GeePaw Hill <geepawhill@geepawhill.org>
2022-07-30 10:51:53 -0400

    Kotlin enabled and converted.

---
+++ desktop/src/org/fgno/game/DesktopLauncher.kt
@@ -0,0 +1,15 @@
+package org.fgno.game
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = Lwjgl3ApplicationConfiguration()
+        config.setForegroundFPS(60)
+        config.setTitle("fgno-game")
+        Lwjgl3Application(FgnoGame(), config)
+    }
+}
