3355e947c95aa02da31ad4e045fc35d89606b60d
GeePaw Hill <geepawhill@geepawhill.org>
2022-07-30 12:45:41 -0400

    Add raindrop rendering, bucket collision, and disposal.

--- core/src/main/org/fgno/game/FgnoGame.kt
+++ core/src/main/org/fgno/game/FgnoGame.kt
@@ -8,9 +8,11 @@ import com.badlogic.gdx.audio.Sound
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.MathUtils
 import com.badlogic.gdx.math.Rectangle
 import com.badlogic.gdx.math.Vector3
 import com.badlogic.gdx.utils.ScreenUtils
+import com.badlogic.gdx.utils.TimeUtils
 
 
 class FgnoGame : ApplicationAdapter() {
@@ -19,22 +21,26 @@ class FgnoGame : ApplicationAdapter() {
     private lateinit var bucketImage: Texture
     private lateinit var dropSound: Sound
     private lateinit var rainMusic: Music
-    private lateinit var batch:SpriteBatch
-    private val bucket = Rectangle(800f/2f-64f/2f,20f,64f,64f)
+    private lateinit var batch: SpriteBatch
+    private val bucket = Rectangle(800f / 2f - 64f / 2f, 20f, 64f, 64f)
 
     private val camera = OrthographicCamera()
+    private val raindrops = com.badlogic.gdx.utils.Array<Rectangle>()
+    private var lastDropTime = 0L
 
     override fun create() {
         dropImage = Texture(com.badlogic.gdx.Gdx.files.internal("droplet.png"))
-        bucketImage  = Texture(Gdx.files.internal("bucket.png"))
+        bucketImage = Texture(Gdx.files.internal("bucket.png"))
         dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
         rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
 
-        rainMusic.isLooping=true
+        rainMusic.isLooping = true
         rainMusic.play()
 
         batch = SpriteBatch()
-        camera.setToOrtho(false,800f,400f)
+        camera.setToOrtho(false, 800f, 400f)
+
+        spawnRaindrop()
     }
 
     override fun render() {
@@ -43,6 +49,9 @@ class FgnoGame : ApplicationAdapter() {
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
         batch.draw(bucketImage, bucket.x, bucket.y);
+        for (raindrop in raindrops) {
+            batch.draw(dropImage, raindrop.x, raindrop.y)
+        }
         batch.end();
 
         if (Gdx.input.isTouched) {
@@ -52,13 +61,41 @@ class FgnoGame : ApplicationAdapter() {
             bucket.x = touchPos.x - 64 / 2
         }
 
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
 
-        if(bucket.x < 0f) bucket.x = 0f;
-        if(bucket.x > 800f - 64f) bucket.x = 800f - 64f;
+        if (bucket.x < 0f) bucket.x = 0f;
+        if (bucket.x > 800f - 64f) bucket.x = 800f - 64f;
+
+        if (TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
+
+        val iter: MutableIterator<Rectangle> = raindrops.iterator()
+        while (iter.hasNext()) {
+            val raindrop = iter.next()
+            raindrop.y -= 200 * Gdx.graphics.deltaTime
+            if (raindrop.y + 64 < 0) iter.remove()
+            if (raindrop.overlaps(bucket)) {
+                dropSound.play();
+                iter.remove();
+            }
+        }
     }
 
     override fun dispose() {
+        dropImage.dispose();
+        bucketImage.dispose();
+        dropSound.dispose();
+        rainMusic.dispose();
+        batch.dispose();
+    }
+
+    private fun spawnRaindrop() {
+        val raindrop = Rectangle()
+        raindrop.x = MathUtils.random(0, 800 - 64).toFloat()
+        raindrop.y = 480f
+        raindrop.width = 64f
+        raindrop.height = 64f
+        raindrops.add(raindrop)
+        lastDropTime = TimeUtils.nanoTime()
     }
 }
