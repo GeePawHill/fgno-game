9f577da63736a567948b04c5f686afdb30afedaf
GeePaw Hill <geepawhill@geepawhill.org>
2022-07-30 11:42:35 -0400

    Desktop folder structure correct.

---
+++ desktop/src/test/org/fgno/game/DesktopSomethingTest.kt
@@ -0,0 +1,12 @@
+package org.fgno.game
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class DesktopSomethingTest {
+
+    @Test
+    fun testDesktopSomething() {
+        assertThat(1).isEqualTo(2)
+    }
+}
